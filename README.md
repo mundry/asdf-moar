20f9d63f780ea09d0ff93dbd1431c94e0e3abe13
mundry <mundry@users.noreply.github.com>
2024-07-23 18:36:25 +0200

    feat: adapt template to moar

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # asdf-moar [![Build](https://github.com/mundry/asdf-moar/actions/workflows/build.yml/badge.svg)](https://github.com/mundry/asdf-moar/actions/workflows/build.yml) [![Lint](https://github.com/mundry/asdf-moar/actions/workflows/lint.yml/badge.svg)](https://github.com/mundry/asdf-moar/actions/workflows/lint.yml)
 
-[moar](https://github.com/walles/moar) plugin for the [asdf version manager](https://asdf-vm.com).
+[moar](https://github.com/walles/moar) plugin for the [asdf version manager](https://asdf-vm.com) and [mise version manager](https://mise.jdx.dev/).
 
 </div>
 
@@ -15,10 +15,7 @@
 
 # Dependencies
 
-**TODO: adapt this section**
-
 - `bash`, `curl`, `tar`, and [POSIX utilities](https://pubs.opengroup.org/onlinepubs/9699919799/idx/utilities.html).
-- `SOME_ENV_VAR`: set this environment variable in your shell config to load the correct version of tool x.
 
 # Install
 
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for moar.
 GH_REPO="https://github.com/walles/moar"
 TOOL_NAME="moar"
 TOOL_TEST="moar --version"
@@ -31,9 +30,36 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if moar has other means of determining installable versions.
 	list_github_tags
+}
+
+get_platform() {
+  local platform
+
+  case "$(uname | tr '[:upper:]' '[:lower:]')" in
+  darwin) platform="darwin" ;;
+  linux) platform="linux" ;;
+  windows) platform="windows" ;;
+  *)
+    fail "Platform '$(uname)' not supported!"
+    ;;
+  esac
+
+  echo -n $platform
+}
+
+get_arch() {
+  local arch
+
+  case "$(uname -m)" in
+  x86_64 | amd64) arch="amd64" ;;
+  aarch64 | arm64) arch="arm" ;;
+  *)
+    fail "Arch '$(uname -m)' not supported!"
+    ;;
+  esac
+
+  echo -n $arch
 }
 
 download_release() {
@@ -41,8 +67,7 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for moar
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$GH_REPO/releases/download/v${version}/${TOOL_NAME}-v${version}-$(get_platform)-$(get_arch)"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -59,9 +84,9 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		cp "$ASDF_DOWNLOAD_PATH"/"${TOOL_NAME}" "$install_path"
+		chmod +x "$install_path"/"${TOOL_NAME}"
 
-		# TODO: Assert moar executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
